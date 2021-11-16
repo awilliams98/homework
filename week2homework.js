22b9589d381e3451d725a5dc24707d12ae77a389
asante williams <asantejones@live.com>
2021-11-16 15:49:42 -0600

    first commit

---
+++ week2homework.js
@@ -0,0 +1,11 @@
+var Ajspizza ;
+
+let numberoftoppings = 15
+
+if (numberoftoppings <=14)  {
+  console.log("The pizza place offers quality,not quantity.")
+}
+else if(numberoftoppings >=14)  {
+  console.log("the pizza place offers a whole lot of pizza.")
+}
+let sentence = "I love" + Ajspizza + "because they have" + numberoftoppings + "toppings!"'
